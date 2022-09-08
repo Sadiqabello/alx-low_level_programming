c9c1a9c83600ac738cce7961f53df040a60afc77
Sadiqabello <sadiqabello0@gmail.com>
2022-09-08 16:07:06 -0700

    Updated 101-quote betty wahala stuffs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1,stderr);
-	return (0);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
 }
