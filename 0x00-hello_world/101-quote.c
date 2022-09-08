45c8e19fc408bca67f3d5a15496978fbe270cbf9
Sadiqabello <sadiqabello0@gmail.com>
2022-09-08 15:57:16 -0700

    betty wahala

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1,stderr);
+	return (0);
+}
