7ba590031b0f01feb87e4dfb97e298c78e0aa9ec
Sadiqabello <sadiqabello0@gmail.com>
2022-09-09 14:39:44 -0700

    Print anphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
+{
+
+	int ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+
+	return (0);
+}
