a4e0c0ec3610cc950ed85b83118b522ed53ad4e8
Sadiqabello <sadiqabello0@gmail.com>
2022-09-09 16:58:20 -0700

    UPDATE 100-print_comb3 betty wahala

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,18 @@ int main(void)
 
 	for (tens = 0; tens <= 9; tens++)
 	{
-	for (ones = tens + 1; ones <= 9; ones++)
-	{
-		putchar(tens + '0');
-		putchar(ones + '0');
-
-	if (tens < 8)
-	{																		putchar(',');
-		putchar(' ');															}														
-	}	
-	}
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+		
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
 	putchar('\n');
 
 	return (0);
