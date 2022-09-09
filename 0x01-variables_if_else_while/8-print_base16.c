bbb97cc146f1357f0989799816b68722021b2d07
Sadiqabello <sadiqabello0@gmail.com>
2022-09-09 16:14:18 -0700

    Print in Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints hexadecimal
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar(10);
+
+	return (0);
+}
