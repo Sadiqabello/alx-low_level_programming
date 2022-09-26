b2d5de0a1d75119f14a9fa7c1c800bb6f9c9cb07
sadiqabello <sadiqabello0@gmail.com>
2022-09-27 00:41:20 +0100

    Edited 0-memset and 1-memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
-/**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: desired value
- * @n: number of bytes to be changed
- *
- * Return: changed array with n bytes
- */
 
+/**
+ * _memset - fill memory with a constant byte
+ * @s: a pointer to a memory area
+ * @b: the byte to write to memory
+ * @n: the number of bytes to write
+ *
+ * Return: a pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a;
+	unsigned int i;
 
-	for (a = 0;a < n; a++)
-	{
-		s[a] = b;
-	}
+	for (i = 0; i < n; ++i)
+		s[i] = b;
 	return (s);
 }
